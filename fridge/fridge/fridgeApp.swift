da06f54491c02975dba2286623bd090b82a7ade4
Aktan Azat <aazat@ucdavis.edu>
2025-04-15 19:41:27 -0700

    test

---
+++ fridge/fridge/fridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fridgeApp.swift
+//  fridge
+//
+//  Created by Aktan Azat on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct fridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
