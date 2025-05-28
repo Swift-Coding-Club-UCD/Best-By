f97f1f95098a20b171907987fda151d6dca4f84f
AGG-104 <gamber104@gmail.com>
2025-05-28 11:32:19 -0700

    camera issue fixed, formatting of app issue appeared

---
+++ fridge/fridge/fridgeTests/fridgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  fridgeTests.swift
+//  fridgeTests
+//
+//  Created by Aktan Azat on 4/15/25.
+//
+
+import Testing
+@testable import fridge
+
+struct fridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
